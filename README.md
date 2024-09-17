48061d2ae1ddeb14cc490af56af364f2443e834c
Aadith_Th <taadith@gmail.com>
2024-09-17 18:27:49 -0500

    created scanToken() func

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Following Robert Nystrom's implementation of jlox/clox in an effort to learn how
 
 jlox:
 1. updates for 09/14\
-    *i.* worked on **Interpreter Framework**\
-    *ii.* a blog post for this leg of work?\
+    *i.* finished **Interpreter Framework**\
+    *ii.* should i start a blog post for this leg of work?\
         &nbsp;&nbsp;&nbsp;&nbsp;*a.* what's next?? an arm??\
         &nbsp;&nbsp;&nbsp;&nbsp;*b.* hmmm maybe, maybe not... this is more of a copy paste project with some challenges that i attempt to solve
 2. updates for 9/16\
-    *i.* worked on **Lexemes and Tokens**\
-    *ii.* working on **The Scanner Class**
+    *i.* finished **Lexemes and Tokens**, **The Scanner Class**\
+    *ii.* started **Recognizing Lexemes**
--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -5,13 +5,78 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import java.com.craftinginterpreters.TokenType.*;
+import java.com.craftinginterpreters.lox.TokenType;
 
 class Scanner {
     private final String src;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
+    // store the raw src code as a simple string
     Scanner(String src) {
         this.src = src;
+    }
+
+    // takes raw src code
+    List<Token> scanTokens() {
+        // scan a single token
+        while (!isAtEnd()) {
+            // we're at the beginning of the next lexeme
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+
+        switch(c) {
+            // single-character tokens
+            case '(':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                addToken(TokenType.RIGHT_PAREN);
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+            case ',':
+                addToken(TokenType.COMMA);
+            case '.':
+                addToken(TokenType.DOT);
+            case '-':
+                addToken(TokenType.MINUS);
+            case '+':
+                addToken(TokenType.PLUS);
+            case ';':
+                addToken(TokenType.SEMICOLON);
+            case '*':
+                addToken(TokenType.STAR);
+        }
+    }
+
+    private boolean isAtEnd() {
+        return current >= src.length();
+    }
+
+    // consumes the next char in the src file and returns it
+    private char advance() {
+        return src.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    // grabs txt of current lexeme and creates a new "Token" for it
+    private void addToken(TokenType type, Object literal) {
+        String txt = src.substring(start, current);
+        tokens.add(new Token(type, txt, literal, line));
     }
 }
