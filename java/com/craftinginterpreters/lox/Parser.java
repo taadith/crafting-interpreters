20dfdf60443dface805d8fc5277cb6d71258c201
Aadith_Th <taadith@gmail.com>
2024-10-03 15:39:06 -0500

    created file, added expression and equality

---
+++ java/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,66 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    // expression -> equality ;
+    private Expr expression() {
+        return equality();
+    }
+
+    // equality -> comparison (( "!=" | "==") comparison)* ;
+    private Expr equality() {
+        Expr expr = comparison();
+
+        // `(( "!=" | "==") comparison)* ;`
+        while(match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd())
+            return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd())
+            current++;
+        return previous();
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == TokenType.EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+}
