3bee9f8e209ddabfa121e0145da020f64b42e1fb
Aadith_Th <taadith@gmail.com>
2024-10-05 19:11:51 -0500

    changes to Interpreter

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,127 @@
 package com.craftinginterpreters.lox;
 
+// declaring itself as a visitor
 class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch(expr.operator.type) {
+            // equality operators
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+
+            // comparison operators:
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                return (double)left <= (double)right;
+
+            // arithmetic operators:
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                // dynamically checking the type of operands
+                if (left instanceof Double && right instanceof Double)
+                    return (double)left + (double)right;
+                if (left instanceof String && right instanceof String)
+                    return (String)left + (String)right;
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        // unreachable
+        return null;
+    }
+
+
+    @Override
+    public Object visitTernaryExpr(Expr.Ternary expr) {
+        Object left = evaluate(expr.left);
+        Object mid = evaluate(expr.mid);
+        Object right = evaluate(expr.right);
+
+        if (expr.op1.type == TokenType.QUESTION && expr.op2.type == TokenType.COLON) {
+            if(isTruthy(left))
+                return mid;
+            return right;
+        }
+
+        // unreachable (?)
+        return null;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null)
+            return true;
+        if (a == null)
+            return false;
+        
+        return a.equals(b);
+    }
     
+    // grouping node has a reference to an inner node...
+    // ... for the expression contained w/in the "()"
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    // converting the literal tree node...
+    // ... into a runtime value is trivial
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        // bc unary expressions are right associative...
+        // ... we first evaluate the operand expression
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                checkNumberOperand(expr.operator, right);
+                // type cast before performing the operation...
+                // ... at runtime before "-" is evaluated ...
+                // ... making this why language is dynamically typed
+                return -(double)right;
+        }
+
+        // unreachable
+        return null;
+    }
+
+    private boolean isTruthy(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj instanceof Boolean)
+            return (boolean)obj;
+        return true;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double)
+            return;
+        throw new RuntimeError(operator, "operand must be a number");
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
 }
