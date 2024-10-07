f3eaf9e6bdcd4385fa160ffe4404a1d77a1bc122
Aadith_Th <taadith@gmail.com>
2024-10-07 18:44:09 -0500

    set up basic environments

--- java/com/craftinginterpreters/lox/Environment.java
+++ java/com/craftinginterpreters/lox/Environment.java
@@ -11,4 +11,15 @@ class Environment {
         // when we add key to the map, we don't check to see if it's already present
         values.put(name, value);
     }
+
+    // looking up an existing variable
+    Object get(Token name) {
+        // if variable is found, return the value bound to it
+        if (values.containsKey(name.lexeme))
+            return values.get(name.lexeme);
+        
+        // important choice to make it a runtime error, not a syntax error or to simply allow for it
+        throw new RuntimeError(name,
+            "undefined variable '" + name.lexeme + "'");
+    }
 }
