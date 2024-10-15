5ff7c3f3ce37a46d7537260c5feac62675f06d1e
Aadith_Th <taadith@gmail.com>
2024-10-15 18:05:29 -0500

    created base of function object

--- java/com/craftinginterpreters/lox/LoxFunction.java
+++ java/com/craftinginterpreters/lox/LoxFunction.java
@@ -4,7 +4,36 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    
     LoxFunction(Stmt.Function declaration) {
         this.declaration = declaration;
+    }
+
+    @Override
+    public String toString() {
+        return "<fn " + declaration.name.lexeme + ">";
+    }
+
+    @Override
+    public int arity() {
+        return declaration.params.size();
+    }
+
+    @Override
+    public Object call(Interpreter interpreter,
+                       List<Object> arguments) {
+
+        // each function call gets its own environment
+        Environment env = new Environment(interpreter.globals);
+
+        // define each param and bind arg value to it
+        for(int i = 0; i < declaration.params.size(); i++)
+            env.define(declaration.params.get(i).lexeme, arguments.get(i));
+        
+        // discards function-local env and restores the...
+        // ... previous one that was active back at the callsite
+        interpreter.executeBlock(declaration.body, env);
+
+        return null;
     }
 }
