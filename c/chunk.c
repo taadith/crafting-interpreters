cb1be7702563c355f120d580c452ea020d41b156
Aadith_Th <taadith@gmail.com>
2024-11-05 14:53:26 -0600

    removed lines

--- c/chunk.c
+++ c/chunk.c
@@ -8,7 +8,6 @@ void initChunk(Chunk* chunk) {
     chunk -> count = 0;
     chunk -> capacity = 0;
     chunk -> code = NULL;
-    chunk -> lines = NULL;
     initRunLengthEncoding(&chunk -> rle);
     initValueArray(&chunk -> constants);
 }
@@ -23,19 +22,15 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
         // double size of arrays (or set to 8)
         chunk -> code = GROW_ARRAY(uint8_t, chunk -> code,
                                    oldCapacity, chunk -> capacity);
-        chunk -> lines = GROW_ARRAY(int, chunk -> lines,
-                                    oldCapacity, chunk -> capacity);
     }
 
     chunk -> code[chunk -> count] = byte;
-    chunk -> lines[chunk -> count] = line;
     writeRunLengthEncoding(&chunk -> rle, line);
     chunk -> count++;
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk -> code, chunk -> capacity);
-    FREE_ARRAY(int, chunk -> lines, chunk -> capacity);
     freeRunLengthEncoding(&chunk -> rle);
     freeValueArray(&chunk -> constants);
     
