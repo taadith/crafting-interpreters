28e839da2a2e58f894c404dcdce2a3fe8a8a7d6d
Aadith_Th <taadith@gmail.com>
2024-11-05 16:49:55 -0600

    enabled dynamic trace exec

--- c/vm.c
+++ c/vm.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
+
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
@@ -12,11 +15,31 @@ void freeVM() {
 }
 
 static InterpretResult run() {
+// reads current byte pointed at by ip...
+// ... and then advances the ip
 #define READ_BYTE() (*vm.ip++)
 
+// reads the next byte from the bytecode...
+// ... treats the resulting # as an index...
+// ... and looks up the corresponding Value in the chunk's constant table
+#define READ_CONSTANT() (vm.chunk -> constants.values[READ_BYTE()])
+
     for(;;) {
+
+// dynamic debugging if flag is defined
+#ifdef DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk -> code));
+#endif
+
+        // first byte of any instruction is the opcode
         uint8_t instruction;
         switch(instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
             case OP_RETURN: {
                 return INTERPRET_OK;
             }
@@ -24,6 +47,7 @@ static InterpretResult run() {
     }
 
 #undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk* ch) {
