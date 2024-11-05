8b90838102a15fe02841b2f5e156d827a7864ef8
Aadith_Th <taadith@gmail.com>
2024-11-05 16:29:43 -0600

    added InterpretResult, interpret(), run()

--- c/vm.c
+++ c/vm.c
@@ -8,3 +8,27 @@ void initVM(VM* vm) {
 void freeVM(VM* vm) {
 
 }
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+
+    for(;;) {
+        uint8_t instruction;
+        switch(instruction = READ_BYTE()) {
+            case OP_RETURN: {
+                return INTERPRET_OK;
+            }
+        }
+    }
+
+#undef READ_BYTE
+}
+
+InterpretResult interpret(VM* vm, Chunk* ch) {
+    vm -> chunk = ch;
+
+    // ip pts to the first byte of code in the chunk
+    // ip pts to the instruction abt to be executed
+    vm -> ip = vm -> chunk -> code;
+    return run();
+}
--- c/vm.h
+++ c/vm.h
@@ -5,9 +5,21 @@
 
 typedef struct {
     Chunk* chunk;
+
+    // instruction ptr (ip) is defined as a addebyte ptr
+    uint8_t* ip;
 } VM;
+
+// results of interpret()
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
 
 void initVM(VM* vm);
 void freeVM(VM* vm);
+
+InterpretResult interpret(Chunk* chunk);
 
 #endif
