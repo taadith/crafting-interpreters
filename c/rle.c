097f6f3cf26f6bdd8b6524ef24a1631ca5e916a6
Aadith_Th <taadith@gmail.com>
2024-11-05 14:11:27 -0600

    made c file for rle

---
+++ c/rle.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include "rle.h"
+#include "memory.h"
+
+// creates a RLE
+void initRunLengthEncoding(RunLengthEncoding* rle) {
+
+}
+
+// writes new data to the RLE
+void writeRunLengthEncoding(RunLengthEncoding* rle, int newData) {
+
+}
+
+// frees the RLE
+void freeRunLengthEncoding(RunLengthEncoding* rle) {
+
+}
+
+// prints out the RLE
+void printRunLengthEncoding(RunLengthEncoding* rle) {
+
+}
