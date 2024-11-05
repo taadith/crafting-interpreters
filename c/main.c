be96806fab659de18e60bae9702a9ceb5d61131c
Aadith_Th <taadith@gmail.com>
2024-11-05 14:49:29 -0600

    added testing for RLEs

--- c/main.c
+++ c/main.c
@@ -1,12 +1,29 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "chunk.h"
+#include "rle.h"
 #include "debug.h"
 
 void testRunLengthEncoding() {
+    printf("starting RunLengthEncoding test...\n");
+
+    RunLengthEncoding rle;
+    initRunLengthEncoding(&rle);
+
+    for(int i = 0; i < 16; i++)
+        writeRunLengthEncoding(&rle, i / 4);
+
+    printRunLengthEncoding(&rle);
+
+    freeRunLengthEncoding(&rle);
     
+    printf("... completed RunLengthEncoding test\n\n");
 }
 
 void testChunk() {
+    printf("started Chunk test...\n");
+
     Chunk chunk;
     initChunk(&chunk);
 
@@ -19,9 +36,12 @@ void testChunk() {
     disassembleChunk(&chunk, "test chunk");
 
     freeChunk(&chunk);
+    
+    printf("... completed Chunk test\n");
 }
 
 int main(int argc, const char* argv[]) {
+    testRunLengthEncoding();
     testChunk();
     return 0;
 }
