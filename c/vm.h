8a98f0f2c2238af4663027802a1b897ee0d98ec3
Aadith_Th <taadith@gmail.com>
2024-11-06 16:51:53 -0600

    added stack

--- c/vm.h
+++ c/vm.h
@@ -2,12 +2,21 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct {
     Chunk* chunk;
 
-    // instruction ptr (ip) is defined as a addebyte ptr
+    // instruction ptr (ip) is defined as a byte ptr
     uint8_t* ip;
+
+    // stack is implemented as a double[]
+    Value stack[STACK_MAX];
+
+    // pts just past the last item
+    Value* stackTop;
 } VM;
 
 // results of interpret()
