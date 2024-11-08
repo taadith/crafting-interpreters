7dbb8af127ddc357802126b2a1b31b6e30069471
Aadith_Th <taadith@gmail.com>
2024-11-07 22:19:29 -0600

    added TokenType and Token

--- c/compiler.c
+++ c/compiler.c
@@ -6,4 +6,19 @@
 
 void compile(const char* src) {
     initScanner(src);
+    int line = -1;
+    for(;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        }
+        else
+            printf("\t| ");
+        
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF)
+            break;
+    }
 }
