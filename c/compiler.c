95e241137bdd0707f0efc9533fe8055b0a0e8103
Aadith_Th <taadith@gmail.com>
2024-11-11 11:42:56 -0600

    did some work on the front end

--- c/compiler.c
+++ c/compiler.c
@@ -1,27 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* src) {
-    initScanner(src);
-    int line = -1;
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
 
-    // loops indefinitely
+Parser parser;
+
+static void errorAt(Token* token, const char* msg) {
+    // exit out of function unless we...
+    // ... haven't entered panic mode yet
+    if (parser.panicMode)
+        return;
+    parser.panicMode = true;
+
+    // print where error occured
+    fprintf(stderr, "[line %d] Error", token -> line);
+
+    // what kind of token is it?
+    if (token -> type == TOKEN_EOF)
+        fprintf(stderr, " at end");
+    else if (token -> type == TOKEN_ERROR) {}
+    else
+        fprintf(stderr, " at '%.*s'", token -> length, token -> start);
+    
+    // print the error msg itself
+    fprintf(stderr, ": %s\n", msg);
+
+    // set the `hadError` flag
+    parser.hadError = true;
+}
+
+// reporting error at previous token
+static void error(const char* msg) {
+    errorAt(&parser.previous, msg);
+}
+
+// reporting error at current token
+static void errorAtCurrent(const char* msg) {
+    errorAt(&parser.current, msg);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+
+    // keep reporting errors until we...
+    // ... reach the end or a non-error token
     for(;;) {
-        // scans one token and then prints it
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        }
-        else
-            printf("   | ");
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+        // here's where the scanner...
+        // ... emits a token to the parser
+        parser.current = scanToken();
 
-        // stops on the "EOF" token or an error token
-        if (token.type == TOKEN_EOF)
+        if (parser.current.type != TOKEN_ERROR)
             break;
+        errorAtCurrent(parser.current.start);
     }
+}
+
+static void consume(TokenType type, const char* msg) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(msg);
+}
+
+bool compile(const char* src, Chunk* chunk) {
+    initScanner(src);
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    // "primes the pump" on the scanner
+    advance();
+
+    // parse a single expression
+    expression();
+
+    // checking for sentinel EOF token
+    consume(TOKEN_EOF, "expected end of expression");
+
+    // returns false if an error occured
+    return !parser.hadError;
 }
