66bbba522bbe674a2fefdfeab1ab34e1f04f6c5b
Aadith_Th <taadith@gmail.com>
2024-11-10 19:12:19 -0600

    can now scan whitespace and comments

--- c/scanner.c
+++ c/scanner.c
@@ -26,6 +26,38 @@ static bool isAtEnd() {
     return *scanner.current == '\0';
 }
 
+// consumes next char and returns it
+static char advance() {
+    scanner.current++;
+    return scanner.current[-1];
+}
+
+// returns the next char w/ no consumption
+static char peek() {
+    return *scanner.current;
+}
+
+// similar to peek but looks at...
+// ... the char past the current one
+static char peekNext() {
+    if (isAtEnd())
+        return '\0';
+    return scanner.current[1];
+}
+
+static bool match(char expected) {
+    // if next character is EOF or not expected
+    if (isAtEnd())
+        return false;
+    if (*scanner.current != expected)
+        return false;
+    
+    // advance the scanner
+    scanner.current++;
+
+    return true;
+}
+
 // constructor-like function for Token
 static Token makeToken(TokenType type) {
     Token token;
@@ -49,7 +81,38 @@ static Token errorToken(const char* msg) {
     return token;
 }
 
+static void skipWhitespace() {
+    for(;;) {
+        char c = peek();
+        switch(c) {
+            case ' ':
+            case '\r':
+            case '\t':
+                advance();
+                break;
+            case '\n':
+                scanner.line++;
+                advance();
+                break;
+            case '/':
+                // a comment goes until the end of the line
+                if (peekNext() == '/') {
+                    while(peek() != '\n' && !isAtEnd())
+                        advance();
+                }
+                else
+                    return;
+                break;
+            default:
+                return;
+        }
+    }
+}
+
 Token scanToken() {
+    // for handling whitespace
+    skipWhitespace();
+
     // pts to current char to remember where the...
     // ... lexeme that is about to be scanned starts
     scanner.start = scanner.current;
@@ -57,6 +120,52 @@ Token scanToken() {
     // check if at the end of the src file
     if (isAtEnd())
         return makeToken(TOKEN_EOF);
+
+    char c = advance();
+
+    switch(c) {
+        // single character tokens
+        case '(':
+            return makeToken(TOKEN_LEFT_PAREN);
+        case ')':
+            return makeToken(TOKEN_RIGHT_PAREN);
+        case '{':
+            return makeToken(TOKEN_LEFT_BRACE);
+        case '}':
+            return makeToken(TOKEN_RIGHT_BRACE);
+        case ';':
+            return makeToken(TOKEN_SEMICOLON);
+        case ',':
+            return makeToken(TOKEN_COMMA);
+        case '.':
+            return makeToken(TOKEN_DOT);
+        case '-':
+            return makeToken(TOKEN_MINUS);
+        case '+':
+            return makeToken(TOKEN_PLUS);
+        case '/':
+            return makeToken(TOKEN_SLASH);
+        case '*':
+            return makeToken(TOKEN_STAR);
+        
+        // one/two character tokens
+        case '!':
+            return makeToken(
+                match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG
+            );
+        case '=':
+            return makeToken(
+                match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL
+            );
+        case '<':
+            return makeToken(
+                match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS
+            );
+        case '>':
+            return makeToken(
+                match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER
+            );
+    }
     
     return errorToken("unexpected character");
 }
