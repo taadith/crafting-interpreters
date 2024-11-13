51fa9767f3c00841f3b57a9d7129878284eb6228
Aadith_Th <taadith@gmail.com>
2024-11-13 16:28:36 -0600

    added handling for OP_GREATER and OP_LESS

--- c/vm.c
+++ c/vm.c
@@ -130,6 +130,14 @@ static InterpretResult run() {
                 break;
             }
 
+            case OP_GREATER:
+                BINARY_OP(BOOL_VAL, >);
+                break;
+            
+            case OP_LESS:
+                BINARY_OP(BOOL_VAL, <);
+                break;
+
             case OP_ADD: {
                 BINARY_OP(NUMBER_VAL, +);
                 break;
