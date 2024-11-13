03454883fd8704fc0ff0e8a3b3637fb90a284bc8
Aadith_Th <taadith@gmail.com>
2024-11-13 17:11:38 -0600

    added useful macros

--- c/object.h
+++ c/object.h
@@ -7,6 +7,15 @@
 // extracts object type tag from a given Value
 #define OBJ_TYPE(value)     (AS_OBJ(value) -> type)
 
+#define IS_STRING(value)    isObjType(value, OBJ_STRING)
+
+// returns the ObjString* ptr
+#define AS_STRING(value)    ((ObjString*) AS_OBJ(value))
+
+// steps through the ObjString* ptr...
+// ... to return the char array itself
+#define AS_CSTRING(value)   (((ObjString*) AS_OBJ(value)) -> chars)
+
 typedef enum {
     OBJ_STRING,
 } ObjType;
@@ -20,5 +29,9 @@ struct ObjString {
     int length;
     char* chars;
 };
+
+static inline bool isObjType(Value value, ObjType type) {
+    return IS_OBJ(value) && (AS_OBJ(value) -> type == type);
+}
 
 #endif
