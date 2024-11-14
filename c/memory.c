c75befa87556a355fbc3a41468428fd064673320
Aadith_Th <taadith@gmail.com>
2024-11-14 09:49:09 -0600

    implemented freeing of objects

--- c/memory.c
+++ c/memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "vm.h"
 
 void* reallocate(void* ptr, size_t oldSize, size_t newSize) {
     // oldSize = 0 && newSize > 0
@@ -19,4 +20,29 @@ void* reallocate(void* ptr, size_t oldSize, size_t newSize) {
         exit(1);
     
     return result;
+}
+
+static void freeObject(Obj* object) {
+    switch(object -> type) {
+        case OBJ_STRING: {
+            ObjString* string = (ObjString*) object;
+            
+            // free the char array...
+            FREE_ARRAY(char, string -> chars, string -> length + 1);
+            // ... and then we free the ObjString
+            FREE(ObjString, object);
+        }
+    }
+}
+
+void freeObjects() {
+    Obj* object = vm.objects;
+
+    // walking a linked list...
+    // ... and freeing its nodes
+    while(object != NULL) {
+        Obj* next = object -> next;
+        freeObject(object);
+        object = next;
+    }
 }
