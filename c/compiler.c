49a7c4e821bec332bf4a4b44f9b36fc007712ece
Aadith_Th <taadith@gmail.com>
2024-11-15 13:50:45 -0600

    hooked up identifier tokens to the expression parser

--- c/compiler.c
+++ c/compiler.c
@@ -279,6 +279,10 @@ static void string(void) {
     emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
+static void variable(void) {
+    namedVariable(parser.previous);
+}
+
 static void unary(void) {
     TokenType operatorType = parser.previous.type;
 
@@ -321,7 +325,7 @@ ParseRule rules[] = {
     [TOKEN_GREATER_EQUAL]   = {NULL,        binary, PREC_COMPARISON},
     [TOKEN_LESS]            = {NULL,        binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL]      = {NULL,        binary, PREC_COMPARISON},
-    [TOKEN_IDENTIFIER]      = {NULL,        NULL,   PREC_NONE},
+    [TOKEN_IDENTIFIER]      = {variable,    NULL,   PREC_NONE},
     [TOKEN_STRING]          = {string,      NULL,   PREC_NONE},
     [TOKEN_NUMBER]          = {number,      NULL,   PREC_NONE},
     [TOKEN_AND]             = {NULL,        NULL,   PREC_NONE},
