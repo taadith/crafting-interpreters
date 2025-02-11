644d56ee86f65454878653ddaf569f5eed0fbb03
Aadith_Th <taadith@gmail.com>
2025-02-10 20:13:56 -0600

    can dynamically grow and free memory

---
+++ c/memory.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "memory.h"
+
+void* reallocate(void* ptr, size_t oldSize, size_t newSize) {
+    if (newSize == 0) {
+        free(ptr);
+        return NULL;
+    }
+
+    void* res = realloc(ptr, newSize);
+    if (res == NULL)
+        exit(1);
+
+    return res;
+}
