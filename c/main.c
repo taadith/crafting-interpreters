176b58c8f67ef5048abc8b12a69822d8c4700b10
Aadith_Th <taadith@gmail.com>
2025-02-10 22:17:16 -0600

    added disassembler

--- c/main.c
+++ c/main.c
@@ -6,7 +6,7 @@ int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
-
+    
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
 
