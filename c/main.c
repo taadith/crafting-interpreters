0eff9fc8e8f1004774b11cd2f41dbc15e48b755c
Aadith_Th <taadith@gmail.com>
2025-02-11 22:46:27 -0600

    moved tests to their own functions

--- c/main.c
+++ c/main.c
@@ -1,8 +1,9 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "rle.h"
 
-int main(int argc, const char* argv[]) {
+void test_chunk_simple(void) {
     Chunk chunk;
     initChunk(&chunk);
 
@@ -14,6 +15,19 @@ int main(int argc, const char* argv[]) {
     
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
+}
 
+void test_rle_simple(void) {
+    RunLengthEncoding rle;
+    initRunLengthEncoding(&rle);
+
+    writeRunLengthEncoding(&rle, 1);
+
+    freeRunLengthEncoding(&rle);
+}
+
+int main(int argc, const char* argv[]) {
+    test_chunk_simple();
+    test_rle_simple();
     return 0;
 }
