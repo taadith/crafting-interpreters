eec97c38ece572d7eabcdb8f587e27142fd3286a
Aadith_Th <taadith@gmail.com>
2025-02-22 22:48:33 -0600

    added disassembler support for OP_CONSTANT_LONG

--- c/debug.c
+++ c/debug.c
@@ -30,38 +30,35 @@ static int constantInstruction(const char* name, Chunk* chunk,
     return offset + 2;
 }
 
+static int constantLongInstruction(const char* name, Chunk* chunk,
+                               int offset) {
+    // grab the constant index
+    uint8_t first_byte_constant_index = chunk -> code[offset + 1];
+    uint8_t second_byte_constant_index = chunk -> code[offset + 2];
+    uint8_t third_byte_constant_index = chunk -> code[offset + 3];
+
+    int constant_index = (first_byte_constant_index << 16) |
+                         (second_byte_constant_index << 8) |
+                         (third_byte_constant_index);
+
+    // print out the name of the opcode and constant index
+    printf("%-16s %4d '", name, constant_index);
+
+    // look up the actual constant value
+    printValue(chunk -> constants.values[constant_index]);
+
+    printf("'\n");
+
+    // skips past opcode and three bytes
+    return offset + 4;
+}
+
 static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
 
     // skips past opcode
     return offset + 1;
 }
-
-// int disassembleInstruction(Chunk* chunk, int offset) {
-//     // prints byte offset of the given instruction...
-//     // ... telling us where in the chunk the instruction is
-//     printf("%04d ", offset);
-// 
-//     if (offset > 0 &&
-//             chunk -> lines[offset] == chunk -> lines[offset - 1]) {
-//         printf("    | ");
-//     }
-//     else {
-//         printf("%4d ", chunk -> lines[offset]);
-//     }
-//     
-//     // read the opcode
-//     uint8_t instruction = chunk -> code[offset];
-//     switch(instruction) {
-//         case OP_CONSTANT:
-//             return constantInstruction("OP_CONSTANT", chunk, offset);
-//         case OP_RETURN:
-//             return simpleInstruction("OP_RETURN", offset);
-//         default:
-//             printf("unknown opcode %d\n", instruction);
-//             return offset + 1;
-//     }
-// }
 
 int disassembleInstructionWithRLE(Chunk* chunk, int offset) {
     // prints byte offset of the given instruction...
@@ -81,6 +78,8 @@ int disassembleInstructionWithRLE(Chunk* chunk, int offset) {
     switch(instruction) {
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_CONSTANT_LONG:
+            return constantLongInstruction("OP_CONSTANT_LONG", chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
