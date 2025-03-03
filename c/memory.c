a3763f8f146d984759b44c625d4e3452123bb616
Aadith_Th <taadith@gmail.com>
2025-03-02 20:15:28 -0600

    added modified reallocate function to remove oldSize param

--- c/memory.c
+++ c/memory.c
@@ -14,3 +14,16 @@ void* reallocate(void* ptr, size_t oldSize, size_t newSize) {
 
     return res;
 }
+
+void* new_realloc(void* ptr, size_t newSize) {
+    if (newSize == 0) {
+        free(ptr);
+        return NULL;
+    }
+
+    void* res = realloc(ptr, newSize);
+    if (res == NULL)
+        exit(1);
+
+    return res;
+}
--- c/memory.h
+++ c/memory.h
@@ -11,9 +11,18 @@
     (type*)reallocate(ptr, sizeof(type) * (oldCount), \
         sizeof(type) * (newCount))
 
+// pretties up `new_realloc`
+#define NEW_GROW_ARRAY(type, ptr, newCount) \
+    (type*)new_realloc(ptr, sizeof(type) * (newCount))
+
 #define FREE_ARRAY(type, ptr, oldCount) \
     reallocate(ptr, sizeof(type) * (oldCount), 0)
 
+#define NEW_FREE_ARRAY(ptr) \
+    new_realloc(ptr, 0);
+
 void* reallocate(void* ptr, size_t oldSize, size_t newSize);
+
+void* new_realloc(void* ptr, size_t newSize);
 
 #endif
