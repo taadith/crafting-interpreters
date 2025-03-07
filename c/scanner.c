78ce2c90154ca71d638983b5660baf532fb912a7
Aadith_Th <taadith@gmail.com>
2025-03-06 22:27:16 -0600

    started on lexical grammar regarding single-char tokens

--- c/scanner.c
+++ c/scanner.c
@@ -50,6 +50,34 @@ Token scanToken(Scanner* scanner) {
     
     if (isAtEnd(scanner))
         return makeToken(scanner, TOKEN_EOF);
+
+    char c = advance();
+
+    switch(c) {
+        // single-character tokens
+        case '(':
+            return makeToken(TOKEN_LEFT_PAREN);
+        case ')':
+            return makeToken(TOKEN_RIGHT_PAREN);
+        case '{':
+            return makeToken(TOKEN_LEFT_BRACE);
+        case '}':
+            return makeToken(TOKEN_RIGHT_BRACE);
+        case ';':
+            return makeToken(TOKEN_SEMICOLON);
+        case ',':
+            return makeToken(TOKEN_COMMA);
+        case '.':
+            return makeToken(TOKEN_DOT);
+        case '-':
+            return makeToken(TOKEN_MINUS);
+        case '+':
+            return makeToken(TOKEN_PLUS);
+        case '/':
+            return makeToken(TOKEN_SLASH);
+        case '*':
+            return makeToken(TOKEN_STAR);
+    }
     
     return errorToken(scanner, "unexpected character");
 }
