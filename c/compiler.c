bbd3f8d6ab560a71b95180f7f0f2319b4e7b8c23
Aadith_Th <taadith@gmail.com>
2025-03-21 22:43:20 -0500

    setting up framework for parsing tokens

--- c/compiler.c
+++ c/compiler.c
@@ -4,6 +4,30 @@
 #include "compiler.h"
 #include "scanner.h"
 
+typedef struct {
+    Token current;
+    Token previous;
+} Parser;
+
+Parser parser;
+
+static void advance() {
+    // steps fwd thru the token stream
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+
+        // we skip all error tokens
+        if (parser.current.type != TOKEN_ERROR)
+            break;
+
+        // parser keeps track of error tokens and ...
+        // ... eventually reports them
+        errorAtCurrent(parser.current.start);
+    }
+}
+
 bool compile(const char* src, Chunk* chunk) {
     initScanner(src);
 
