1fe2a15588c8ee2e626905fa52fdbe5ba2af8e38
Aadith_Th <taadith@gmail.com>
2025-03-23 12:55:05 -0500

    implemented some objects

---
+++ c/object.h
@@ -0,0 +1,37 @@
+#ifndef clox_object_h
+#define clox_object_h
+
+#include "common.h"
+#include "value.h"
+
+// extracts the object type tag...
+// ... from a given Value
+#define OBJ_TYPE(value)         (AS_OBJ(value) -> type)
+
+// object type checking
+#define IS_STRING(value)        isObjType(value, OBJ_STRING)
+
+// converting object or C value to it's respective C value or object
+#define AS_STRING(value)        ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)       (((ObjString*)AS_OBJ(value)) -> chars)
+
+typedef enum {
+    OBJ_STRING,
+} ObjType;
+
+struct Obj {
+    ObjType type;
+};
+
+struct ObjString {
+    Obj obj;
+    int length;
+    char* chars;
+};
+
+// helper function for checking object type
+static inline bool isObjType(Value value, ObjType type) {
+    return IS_OBJ(value) && AS_OBJ(value) -> type == type;
+}
+
+#endif
